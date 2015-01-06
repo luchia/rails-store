029af5e9e63c11fe8e6edfff5e5b5622adcee7fd
Luchia Bloomfield <hello@luchia.com.au>
2015-01-06 15:53:48 +1000

    module seven

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -1,49 +1,32 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionController::TestCase
-  setup do
-    @product = products(:one)
-  end
+	setup do
+		@product = products(:one)
+		@product_update = {
+			name: 'Test Product',
+			summary: 'Test Product Description',
+			image_url: 'test_product.jpg',
+			price: 9.95
+		}
+	end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:products)
-  end
+	test "should create product" do
+		assert_difference('Product.count') do
+			post :create, product: @product_update
+		end
+		assert_redirected_to product_path(assigns(:product))
+	end
 
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
+	test "should update product" do
+		patch :update, id: @product, product: @product_update
+		assert_redirected_to product_path(assigns(:product))
+	end
 
-  test "should create product" do
-    assert_difference('Product.count') do
-      post :create, product: { image_url: @product.image_url, name: @product.name, price: @product.price, summary: @product.summary, text: @product.text }
-    end
-
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should show product" do
-    get :show, id: @product
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, id: @product
-    assert_response :success
-  end
-
-  test "should update product" do
-    patch :update, id: @product, product: { image_url: @product.image_url, name: @product.name, price: @product.price, summary: @product.summary, text: @product.text }
-    assert_redirected_to product_path(assigns(:product))
-  end
-
-  test "should destroy product" do
-    assert_difference('Product.count', -1) do
-      delete :destroy, id: @product
-    end
-
-    assert_redirected_to products_path
-  end
+	test "should destroy product" do
+		assert_difference('Product.count', -1) do
+			delete :destroy, id: @product
+		end
+		assert_redirected_to products_path
+	end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,52 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	fixtures :products
+
+	test "product attributes must not be empty" do
+		product = Product.new
+		assert product.invalid?
+		assert product.errors[:name].any?
+		assert product.errors[:summary].any?
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+	end
+
+	test "product price must be positive" do
+		product = Product.new(name: 'Ruler', summary: 'Ruler description', image_url: 'ruler.jpg')
+		product.price = -1
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+		product.price = 0
+		assert product.invalid?
+		assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+
+		product.price = 1
+		assert product.valid?
+	end
+
+	def new_product(image_url)
+		Product.new(name: 'Eraser', summary: 'Eraser description', price: 1, image_url: image_url)
+	end
+
+	test "image url" do
+		ok = %w{ eraser.gif eraser.jpg eraser.png ERASER.JPG ERASER.Jpg http://a.b/c/d/eraser.gif }
+		bad = %w{ eraser.doc eraser.gif/plus eraser.gif.plus }
+
+		ok.each do |name|
+			assert new_product(name).valid?, "#{name} should be valid"
+		end
+
+		bad.each do |name|
+			assert new_product(name).invalid?, "#{name} shouldn't be valid"
+		end
+	end
+
+	test "product is not valid without a unique name" do
+		product = Product.new( name: products(:ruby).name, summary: "yyy", price: 1, image_url: "fred.gif")
+
+		assert product.invalid?
+		assert_equal ["has already been taken"], product.errors[:name]
+	end
 end
